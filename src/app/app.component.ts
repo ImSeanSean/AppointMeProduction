2dd5118e144035b6c862660897bcc9203dc49487
Knight Agojo <Lancecuenca90@gmail.com>
2024-06-02 05:57:07 +0800

    Added things but Still confused
    I dont know if putting the button in App.component.html is right, so I added CSS in the appcomponent.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,23 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router, RouterOutlet } from '@angular/router';
 import { UsertypedialogComponent } from './matdialogs/usertypedialog/usertypedialog.component';
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = "AppointMe"
+  title = 'AppointMe';
 
-  constructor(private router: Router, public dialog: MatDialog){}
+  constructor(private router: Router, public dialog: MatDialog) {}
 
-  redirectToHomePage(){
-    this.router.navigate([''])
+  redirectToHomePage() {
+    this.router.navigate(['']);
   }
 
   openLoginDialog(): void {
@@ -24,10 +26,34 @@ export class AppComponent {
       width: '300px',
     });
 
-    dialogRef.componentInstance.userType.subscribe((selectedUserType: string) => {
-     this.router.navigate(['./'+ selectedUserType + '/login'])
+    dialogRef.componentInstance.userType.subscribe(
+      (selectedUserType: string) => {
+        this.router.navigate(['./' + selectedUserType + '/login']);
+      }
+    );
+  }
+
+  generatePDF() {
+    const elementToPrint: any = document.getElementById(
+      'theContentofViewFinished'
+    ); // The HTML element to become a PDF. The HTMl class you want to convert.
+
+    html2canvas(elementToPrint, { scale: 2 }).then((canvas) => {
+      const pdf = new jsPDF();
+      pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 211, 290);
+
+      pdf.setProperties({
+        title: 'My Documentation PDF',
+        subject: 'PDF from html with Angular',
+        author: 'TeacherName?',
+      });
+
+      pdf.setFontSize(12);
+      pdf.text('RandomTEXT', 14, 22);
+
+      pdf.save('myFile.pdf');
     });
   }
 }
 // export const mainPort = "http://localhost/appointme";
-export const mainPort = "https://appointme.site";
+export const mainPort = 'https://appointme.site';
